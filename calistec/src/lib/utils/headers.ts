a551b7445b9359fd25c774839e46eccc25e4db0f
Braulio <braulio.quine@utec.edu.pe>
2026-06-23 16:58:42 -0500

    add headers for tables and naming tolerance

---
+++ calistec/src/lib/utils/headers.ts
@@ -0,0 +1,19 @@
+export const Exercise_TABLE_HEADERS = [
+    "Ejercicio",
+    "Enfoque",
+    "Dificultad",
+    // eventos
+    "Acciones"
+]
+
+export const Routine_TABLE_HEADERS = [
+    "Rutina",
+    "Descripción",
+    "Ejercicios seleccionados",
+    "Hora de inicio",
+    "Hora de fin",
+    "Creado",
+    "Actualizado",
+    // eventos
+    "Acciones"
+]
---
+++ calistec/src/lib/utils/namingTolerance.ts
@@ -0,0 +1,3 @@
+export const EXERCISE_TABLE = "exercises"
+export const ROUTINE_TABLE = "routines"
+export const EXERCISE_ROUTINE_TABLE = "exercises-routines"
