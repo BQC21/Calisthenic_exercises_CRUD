0651a68a897d2e9990225d384cdc16e6e68a9cb1
Braulio <braulio.quine@utec.edu.pe>
2026-06-24 10:34:15 -0500

    add folder to code often icons

---
+++ calistec/src/features/view/components/icons/AddCloseIcon.tsx
@@ -0,0 +1,7 @@
+export function AddCloseIcon() {
+    return (
+        <svg aria-hidden="true" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 6l12 12M18 6 6 18" />
+        </svg>
+    );
+}
