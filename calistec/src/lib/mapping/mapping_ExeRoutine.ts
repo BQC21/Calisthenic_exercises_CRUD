460d752a17d5899eeda666b017d331cc8a5f70b0
Braulio <braulio.quine@utec.edu.pe>
2026-06-26 15:44:26 -0500

    add option to extrato exercise and routine info from supabase correct table names (ejercicios & rutinas)

--- calistec/src/lib/mapping/mapping_ExeRoutine.ts
+++ calistec/src/lib/mapping/mapping_ExeRoutine.ts
@@ -10,9 +10,9 @@ export function createExercise_RoutineFormStateFromExercise_Routine(
 ): Exercise_RoutineFormState{
     return {
         ejercicio_id: exercise_routine.ejercicio_id,
-        exercise_info: exercise_routine.exercise_info!,
+        exercise_info: exercise_routine.exercise_info,
         rutina_id: exercise_routine.rutina_id,
-        routine_info: exercise_routine.routine_info!,
+        routine_info: exercise_routine.routine_info,
     }
 }
 
@@ -26,12 +26,16 @@ export function mapSupabaseRowToExercise_Routine(
                     ? mapSupabaseRowToExe(row.exercise_info as SupabaseExerciseRow)
                         : row.exercises
                         ? mapSupabaseRowToExe(row.exercises as SupabaseExerciseRow)
+                        : row.ejercicios
+                        ? mapSupabaseRowToExe(row.ejercicios as SupabaseExerciseRow)
                             : undefined,
         rutina_id: row.rutina_id?.toString() || "",  
         routine_info: row.routine_info
                     ? mapSupabaseRowToRoutine(row.routine_info as SupabaseRoutineRow)
                     : row.routines
                         ? mapSupabaseRowToRoutine(row.routines as SupabaseRoutineRow)
+                        : row.rutinas
+                        ? mapSupabaseRowToRoutine(row.rutinas as SupabaseRoutineRow)
                         : undefined,        
     }
 }
